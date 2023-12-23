445654cbc19937dd9215e33551ffc36e8e421027
Deborah Druine <deborahdruine@gmail.com>
2023-12-23 20:53:02 +0100

    Reinitialisation app django

--- LITRevu/wsgi.py
+++ LITRevu/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for merchex project.
+WSGI config for LITRevu project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'merchex.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LITRevu.settings')
 
 application = get_wsgi_application()
